47dec62c50c29a48c56c7aa304192951bc52018c
TakenoriYasugi <takenori.yasugi.cs@gmail.com>
2023-11-09 15:27:14 +0900

    メインのカラーを指定

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import { Button, CssBaseline, Typography } from '@mui/material';
+import { Button, CssBaseline, ThemeProvider, Typography, createTheme } from '@mui/material';
 import Layout from './layout/Layout';
 import Post from './uiparts/Post';
 import { ButtonNavigationLabel } from './constants/Constants';
@@ -14,23 +14,36 @@ import ButtonAppBar from './uiparts/ButtonAppBar';
 function App() {
   // 画面下部のナビゲーションの状態管理
   const [currentButtonNavigation, setCurrentButtonNavigation] = useState<ButtonNavigationLabel>(ButtonNavigationLabel.Home);
+  
+  const theme = createTheme(
+    {
+      palette: {
+        primary: {
+          main: "#121858",
+        }
+      }
+    }
+  );
+  
   return (
-    <div className="App">
-      <CssBaseline/>
-      <header className="App-header">
-        <ButtonAppBar/>
-      </header>
-      <main>
-        <Layout>
-          {currentButtonNavigation === ButtonNavigationLabel.Home && <Home/>}
-          {currentButtonNavigation === ButtonNavigationLabel.Favorite && <Typography>Favo</Typography>}
-          {currentButtonNavigation === ButtonNavigationLabel.Search && <Typography>Search</Typography>}
-        </Layout>
-      </main>
-      <footer>
-        <ButtonMenu value={currentButtonNavigation} setValue={setCurrentButtonNavigation}/> 
-      </footer>
-    </div>
+    <ThemeProvider theme={theme}>
+      <div className="App">
+        <CssBaseline/>
+        <header className="App-header">
+          <ButtonAppBar/>
+        </header>
+        <main>
+          <Layout>
+            {currentButtonNavigation === ButtonNavigationLabel.Home && <Home/>}
+            {currentButtonNavigation === ButtonNavigationLabel.Favorite && <Typography>Favo</Typography>}
+            {currentButtonNavigation === ButtonNavigationLabel.Search && <Typography>Search</Typography>}
+          </Layout>
+        </main>
+        <footer>
+          <ButtonMenu value={currentButtonNavigation} setValue={setCurrentButtonNavigation}/> 
+        </footer>
+      </div>
+    </ThemeProvider>
   );
 }
 
