bbaa9f6b6e0d464ae15ac3f59c7ca3c4b6c453c6
TakenoriYasugi <takenori.yasugi.cs@gmail.com>
2023-11-11 14:40:37 +0900

    仮のモーダルを配置

--- src/uiparts/PostFAB.tsx
+++ src/uiparts/PostFAB.tsx
@@ -1,14 +1,83 @@
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+import Modal from '@mui/material/Modal';
 import { Fab } from "@mui/material";
 import ChatIcon from '@mui/icons-material/Chat';
 
 const PostFAB = () => {
+    const [open, setOpen] = React.useState(false);
+    const handleOpen = () => setOpen(true);
+    const handleClose = () => setOpen(false);
+
     return (
         <>
-            <Fab color="primary" aria-label="add">
+            <Fab color="primary" aria-label="add" onClick={handleOpen}>
                 <ChatIcon />
             </Fab>
+            
+            {/* 押下時に表示する投稿用モーダル */}
+            <div>
+                <Modal
+                    open={open}
+                    onClose={handleClose}
+                    aria-labelledby="modal-modal-title"
+                    aria-describedby="modal-modal-description"
+                >
+                    <Box sx={style}>
+                        <Typography id="modal-modal-title" variant="h6" component="h2">
+                            Text in a modal
+                        </Typography>
+                        <Typography id="modal-modal-description" sx={{ mt: 2 }}>
+                            Duis mollis, est non commodo luctus, nisi erat porttitor ligula.
+                        </Typography>
+                    </Box>
+                </Modal>
+            </div>
       </>
     );
+
+    const PostModal = () => {
+  
+
+  return (
+    <div>
+      <Modal
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+      >
+        <Box sx={style}>
+          <Typography id="modal-modal-title" variant="h6" component="h2">
+            Text in a modal
+          </Typography>
+          <Typography id="modal-modal-description" sx={{ mt: 2 }}>
+            Duis mollis, est non commodo luctus, nisi erat porttitor ligula.
+          </Typography>
+        </Box>
+      </Modal>
+    </div>
+  );
 }
+
+}
+
+
+const style = {
+  position: 'absolute' as 'absolute',
+  top: '50%',
+  left: '50%',
+  transform: 'translate(-50%, -50%)',
+  width: 400,
+  bgcolor: 'background.paper',
+  border: '2px solid #000',
+  boxShadow: 24,
+  p: 4,
+};
+
+
+// 投稿用のモーダル
 
 export default PostFAB;
