59759a85f4a39600c2c192e0d35e84af7cba9b39
TakenoriYasugi <takenori.yasugi.cs@gmail.com>
2023-11-12 10:49:00 +0900

    投稿モーダルの調整中

--- src/uiparts/PostFAB.tsx
+++ src/uiparts/PostFAB.tsx
@@ -3,7 +3,7 @@ import Box from '@mui/material/Box';
 import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
 import Modal from '@mui/material/Modal';
-import { Fab } from "@mui/material";
+import { Divider, Fab, Slide, Stack, TextField } from "@mui/material";
 import ChatIcon from '@mui/icons-material/Chat';
 
 const PostFAB = () => {
@@ -11,73 +11,47 @@ const PostFAB = () => {
     const handleOpen = () => setOpen(true);
     const handleClose = () => setOpen(false);
 
-    return (
-        <>
-            <Fab color="primary" aria-label="add" onClick={handleOpen}>
-                <ChatIcon />
-            </Fab>
-            
-            {/* 押下時に表示する投稿用モーダル */}
-            <div>
-                <Modal
-                    open={open}
-                    onClose={handleClose}
-                    aria-labelledby="modal-modal-title"
-                    aria-describedby="modal-modal-description"
-                >
-                    <Box sx={style}>
-                        <Typography id="modal-modal-title" variant="h6" component="h2">
-                            Text in a modal
-                        </Typography>
-                        <Typography id="modal-modal-description" sx={{ mt: 2 }}>
-                            Duis mollis, est non commodo luctus, nisi erat porttitor ligula.
-                        </Typography>
-                    </Box>
-                </Modal>
-            </div>
-      </>
-    );
+    const style = {
+        position: 'absolute',
+        bottom: 0,
+        left: '50%',
+        transform: 'translateX(-50%)',
+        width: 400,
+        bgcolor: 'background.paper',
+        border: '2px solid #000',
+        boxShadow: 24,
+        p: 4,
+    };
 
-    const PostModal = () => {
-  
+return (
+    <>
+        <Fab color="primary" aria-label="add" onClick={handleOpen}>
+            <ChatIcon />
+        </Fab>
 
-  return (
-    <div>
-      <Modal
-        open={open}
-        onClose={handleClose}
-        aria-labelledby="modal-modal-title"
-        aria-describedby="modal-modal-description"
-      >
-        <Box sx={style}>
-          <Typography id="modal-modal-title" variant="h6" component="h2">
-            Text in a modal
-          </Typography>
-          <Typography id="modal-modal-description" sx={{ mt: 2 }}>
-            Duis mollis, est non commodo luctus, nisi erat porttitor ligula.
-          </Typography>
-        </Box>
-      </Modal>
-    </div>
-  );
-}
+        <Modal
+            open={open}
+            onClose={handleClose}
+            aria-labelledby="modal-modal-title"
+            aria-describedby="modal-modal-description"
+            // モーダルのスタイルを調整して全画面を占めるようにする
+            sx={{ display: 'flex', alignItems: 'flex-end', justifyContent: 'center' }}
+        >
+            <Box sx={style}>
+                <Typography id="modal-modal-title" variant="h6" component="h2">
+                    {/* モーダルのタイトル */}
+                </Typography>
+                <Divider/>
+                <TextField multiline fullWidth size="medium" label="今はどんな気分？"/>
+                <Stack direction={'row-reverse'}>
+                    <Button variant="contained" sx={{m: 1}} onClick={handleClose}>エコー！</Button>
+                    <Button variant="outlined" sx={{m: 1}} onClick={handleClose}>キャンセル</Button>
+                </Stack>
+            </Box>
+        </Modal>
+    </>
+);
 
 }
-
-
-const style = {
-  position: 'absolute' as 'absolute',
-  top: '50%',
-  left: '50%',
-  transform: 'translate(-50%, -50%)',
-  width: 400,
-  bgcolor: 'background.paper',
-  border: '2px solid #000',
-  boxShadow: 24,
-  p: 4,
-};
-
-
-// 投稿用のモーダル
 
 export default PostFAB;
