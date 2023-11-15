713c0ca07019d025258b18b73ccdf4e244f4c71d
TakenoriYasugi <takenori.yasugi.cs@gmail.com>
2023-11-15 16:09:38 +0900

    リアクションをPopoverで表示するように修正

--- src/uiparts/Post.tsx
+++ src/uiparts/Post.tsx
@@ -1,6 +1,6 @@
 // タイムラインに流れる投稿
 
-import { Card, CardContent, Divider, Grow, IconButton, Zoom } from "@mui/material";
+import { Card, CardActionArea, CardContent, Collapse, Divider, Grow, IconButton, Popover, Zoom } from "@mui/material";
 // import '../css/post.css';
 import { useState, useRef, useEffect } from "react";
 
@@ -8,6 +8,8 @@ import ThumbUpAltIcon from '@mui/icons-material/ThumbUpAlt';
 import ThumbDownAltIcon from '@mui/icons-material/ThumbDownAlt';
 import EmojiEmotionsIcon from '@mui/icons-material/EmojiEmotions';
 import SentimentVeryDissatisfiedIcon from '@mui/icons-material/SentimentVeryDissatisfied';
+import FavoriteIcon from '@mui/icons-material/Favorite';
+import PriorityHighIcon from '@mui/icons-material/PriorityHigh';
 
 const Post = ({text} : {text: string}) => {
 
@@ -38,25 +40,60 @@ const Post = ({text} : {text: string}) => {
         observer.disconnect();
       };
     }, []);
+
+    const [isReactionOpen, setIsReactionOpen] = useState(false);
+    const [anchorEl, setAnchorEl] = useState<HTMLButtonElement | null>(null);
     
+    const handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+      setIsReactionOpen(true);
+      setAnchorEl(event.currentTarget)
+    }
+
+    const handleClose = () => {
+      setIsReactionOpen(false);
+      setAnchorEl(null);
+    }
+
     // Zoomコンポーネントを使用してアニメーションを適用
     return (
-      <div ref={cardRef}>
-        <Zoom in={checked} style={{ transformOrigin: "left" }} timeout={800}>
-            <Card className="post" sx={{p: 2, m: 3}}>
-                <CardContent>
-                    {text}
-                </CardContent>
-                <Divider/>
-                <IconButton><ThumbUpAltIcon/></IconButton>
-                <IconButton><EmojiEmotionsIcon/></IconButton>
-                <IconButton><SentimentVeryDissatisfiedIcon/></IconButton>
-                <IconButton><ThumbDownAltIcon/></IconButton>
+      <>
+        <div ref={cardRef}>
+          <Zoom in={checked} style={{ transformOrigin: "left" }} timeout={800}>
+              <Card className="post" sx={{p: 2, m: 3}}>
+                <CardActionArea onClick={handleClick}>
+                  <CardContent>
+                      {text}
+                  </CardContent>
+                  <Divider/>
+                </CardActionArea>
+              </Card>
+          </Zoom>
+        </div>
 
-
-            </Card>
-        </Zoom>
-      </div>
+        <Popover
+          open={isReactionOpen}
+          anchorEl={anchorEl}
+          onClose={handleClose}
+          anchorOrigin={{
+            vertical: 'bottom',
+            horizontal: 'left',
+          }}
+          transformOrigin={{
+            vertical: 'top',
+            horizontal: 'left',
+          }}>
+          <Card>
+            <CardContent>
+              <IconButton><FavoriteIcon sx={{color: "#ff1744"}}/></IconButton>
+              <IconButton><ThumbUpAltIcon sx={{color: "#ffc107"}}/></IconButton>
+              <IconButton><EmojiEmotionsIcon sx={{color: "#ff9800"}}/></IconButton>
+              <IconButton><SentimentVeryDissatisfiedIcon sx={{color: "#ff9800"}}/></IconButton>
+              <IconButton><PriorityHighIcon sx={{color: "red"}}/></IconButton>
+              <IconButton><ThumbDownAltIcon sx={{color: "blue"}}/></IconButton>
+            </CardContent>
+          </Card>
+        </Popover>
+      </>
     );
   }
 
