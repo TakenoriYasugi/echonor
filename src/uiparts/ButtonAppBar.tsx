ba814b7e78a330efcc045fbd00f9d6c15a36623a
TakenoriYasugi <takenori.yasugi.cs@gmail.com>
2023-11-16 15:57:09 +0900

    フリーズの原因になっていた箇所を修正

--- src/uiparts/ButtonAppBar.tsx
+++ src/uiparts/ButtonAppBar.tsx
@@ -7,7 +7,7 @@ import Button from '@mui/material/Button';
 import IconButton from '@mui/material/IconButton';
 import MenuIcon from '@mui/icons-material/Menu';
 
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { CheckUserLoggedIn } from '../util/Authenticator';
 import { Logout } from '@mui/icons-material';
 import LogoutButton from './LogoutButton';
@@ -16,7 +16,14 @@ const ButtonAppBar = ({title}: {title: string}) => {
 
   const [isLoggedIn, setIsLoggedIn] = useState<boolean>(false);
 
-  CheckUserLoggedIn().then(isLoggedIn => isLoggedIn ? setIsLoggedIn(true) : setIsLoggedIn(false));
+  useEffect( () => {
+    let isMounted = true; // マウント状態を追跡するフラグ
+    CheckUserLoggedIn().then(isLoggedIn => isLoggedIn ? setIsLoggedIn(true) : setIsLoggedIn(false));
+
+    return () => {
+      isMounted = false; // アンマウント時にフラグを変更
+    };
+  }, []);
 
   return (
     <Box sx={{ flexGrow: 1 }}>
