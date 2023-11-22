476964b075f44ab1b447438de091758d1774f7e6
TakenoriYasugi <takenori.yasugi.cs@gmail.com>
2023-11-22 16:13:56 +0900

    ハンバーガーメニューを仮作成

--- src/uiparts/ButtonAppBar.tsx
+++ src/uiparts/ButtonAppBar.tsx
@@ -11,6 +11,7 @@ import { useEffect, useState } from 'react';
 import { CheckUserLoggedIn } from '../util/Authenticator';
 import { Logout } from '@mui/icons-material';
 import LogoutButton from './LogoutButton';
+import { Divider, Drawer, List, ListItem, ListItemButton, ListItemText } from '@mui/material';
 
 const ButtonAppBar = ({title}: {title: string}) => {
 
@@ -24,27 +25,75 @@ const ButtonAppBar = ({title}: {title: string}) => {
       isMounted = false; // アンマウント時にフラグを変更
     };
   }, []);
+  const [mobileOpen, setMobileOpen] = React.useState(false);
+
+  const handleDrawerToggle = () => {
+    setMobileOpen((prevState) => !prevState);
+  };
+
+  const drawerWidth = 240;
+
+  const navItems = [
+    "会員情報",
+    "利用規約"
+  ]
+  // スマホで閲覧した際に表示するハンバーガーメニュー
+  const drawer = (
+    <Box onClick={handleDrawerToggle} sx={{ textAlign: 'center' }}>
+      <Typography variant="h6" sx={{ my: 2 }}>
+        Menu
+      </Typography>
+      <Divider />
+      <List>
+        {navItems.map((item) => (
+          <ListItem key={item} disablePadding>
+              <ListItemButton sx={{ textAlign: 'center' }}>
+                <ListItemText primary={item}/>
+              </ListItemButton>
+           </ListItem>
+        ))}
+      </List>
+      {isLoggedIn ? <LogoutButton/> : <Button variant="contained">Login</Button>}
+    </Box>
+  );
 
   return (
-    <Box sx={{ flexGrow: 1 }}>
-      <AppBar position="fixed">
-        <Toolbar>
-          <IconButton
-            size="large"
-            edge="start"
-            color="inherit"
-            aria-label="menu"
-            sx={{ mr: 2 }}
-          >
-            <MenuIcon />
-          </IconButton>
-          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-            {title}
-          </Typography>
-          {isLoggedIn ? <LogoutButton/> : <Button variant="contained">Login</Button>}
-        </Toolbar>
-      </AppBar>
-    </Box>
+    <>
+      <Box sx={{ flexGrow: 1 }}>
+        <AppBar position="fixed">
+          <Toolbar>
+            <IconButton
+              size="large"
+              edge="start"
+              color="inherit"
+              aria-label="menu"
+              sx={{ mr: 2 }}
+              onClick={handleDrawerToggle}
+            >
+              <MenuIcon />
+            </IconButton>
+            <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+              {title}
+            </Typography>
+          </Toolbar>
+        </AppBar>
+      </Box>
+
+      <Drawer
+      variant="temporary"
+      open={mobileOpen}
+      onClose={handleDrawerToggle}
+      ModalProps={{
+        keepMounted: true, // Better open performance on mobile.
+      }}
+      sx={{
+        display: { xs: 'block', sm: 'none' },
+        '& .MuiDrawer-paper': { boxSizing: 'border-box', width: drawerWidth },
+      }}
+      >
+        {drawer}
+      </Drawer>
+    </>
   );
 }
 
