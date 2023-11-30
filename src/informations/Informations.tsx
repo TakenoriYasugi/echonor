9ddab355592f3b4fae3ba48ed05d7656d8f12dcf
TakenoriYasugi <takenori.yasugi.cs@gmail.com>
2023-11-30 15:10:53 +0900

    表示用の外観を整理。日付順にソート。

--- src/informations/Informations.tsx
+++ src/informations/Informations.tsx
@@ -2,16 +2,29 @@ import { Box, Card, Typography } from "@mui/material";
 import { informationsData } from "./informationsData";
 
 const Informations = () => {
+    const sortedInformationsData = informationsData.sort((a, b) => {
+        if (a.date > b.date) {
+            return -1;
+        } else {
+            return 1;
+        }
+    });
+
     return (
         <Card sx={{ maxWidth: 345, p: 2}}>
             <Typography gutterBottom variant="h5" component="div">
                 お知らせ
             </Typography>
             <Box sx={{maxHeight: "300px", overflowY: "auto"}}>
-                {informationsData.map((data) => (
-                    <Typography fontSize={15} variant="body2" color="text.secondary">
-                        {data.date}{data.content}
-                    </Typography>
+                {sortedInformationsData.map((data) => (
+                    <Card sx={{ p: 1, m: 1 }}>
+                        <Typography fontSize={12} variant="body2">
+                            {data.date}
+                        </Typography>
+                        <Typography fontSize={15} variant="body2">
+                            {data.content}
+                        </Typography>
+                    </Card>
                 ))}
             </Box>
         </Card>
