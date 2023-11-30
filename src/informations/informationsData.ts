9eb022a787dede05105b98221082c2340ebfdae6
TakenoriYasugi <takenori.yasugi.cs@gmail.com>
2023-11-30 16:31:04 +0900

    お知らせを更新

--- src/informations/informationsData.ts
+++ src/informations/informationsData.ts
@@ -3,4 +3,5 @@ export type InformationsData = {date: string, content: string};
 export const informationsData: InformationsData[] = [
     {date: "2023/11/30 15:00", content: "お知らせ欄を作成しました。画面上部のアイコンから表示できます。"},
     {date: "2023/11/30 02:00", content: "サポートへのリンクを作成しました。メニューの「サポート」から表示できます。"},
+    {date: "2023/11/30 16:30", content: "今後の更新予定です。①無限スクロールの実装(過去の投稿の非同期取得) ②検索機能 ③集会場"},
 ];
