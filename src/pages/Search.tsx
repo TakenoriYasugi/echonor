1abb68143760fe09ff43bef7565fc485e19bd04f
TakenoriYasugi <takenori.yasugi.cs@gmail.com>
2023-12-02 19:23:52 +0900

    検索機能一旦実装完了

--- src/pages/Search.tsx
+++ src/pages/Search.tsx
@@ -1,4 +1,4 @@
-import { Box, Button, TextField } from "@mui/material";
+import { Box, Button, FormControl, FormControlLabel, FormLabel, Radio, RadioGroup, TextField } from "@mui/material";
 import { useState } from "react";
 import Home from "./Home";
 import { API, graphqlOperation } from "aws-amplify";
@@ -8,18 +8,34 @@ import { listPosts, listPostsByKeyWords } from "../graphql/queries";
 import Post from "../uiparts/Post";
 import { formatDate } from "../util/Format";
 import { set } from "zod";
+import { GraphQLResult } from "@aws-amplify/api";
+import Observable from "zen-observable";
 
 const Search = () => {
     const [searchWords, setSearchWords] = useState<string>("")
     const [posts, setPosts] = useState([]);
+    const [searchMode, setSearchMode] = useState<"and" | "or">("and");
+
+    const handleSearchModeChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setSearchMode((event.target as HTMLInputElement).value as "and" | "or");
+    };
 
     const handleSearch = () => {
         
         // searchWordsを半角、全角スペースで区切って、配列にする
         const searchWordsArray = searchWords.split(/[\s　]+/);
 
+        // searchWordsArrayから、全角スペース、半角スペース、空文字を削除する
+        for (let i = 0; i < searchWordsArray.length; i++) {
+            if (searchWordsArray[i] === "" || searchWordsArray[i] === " " || searchWordsArray[i] === "　") {
+                searchWordsArray.splice(i, 1);
+                i--;
+            }
+        }
+
         // 配列の中身が空の場合、何もしない。
         if (searchWordsArray.length === 0) {
+            setPosts([]);
             return;
         }
 
@@ -29,26 +45,46 @@ const Search = () => {
     const fetchPosts = async (searchWordsArray: string[], limit: number) => {
         try {
             const keyword = searchWordsArray[0];
-            const postData = await API.graphql(graphqlOperation(listPostsByKeyWords, {keyword: keyword, limit: limit}));
+            let postData: GraphQLResult<any> | Observable<object> | undefined;
+            if (searchMode === "and") {
+                postData = await API.graphql(graphqlOperation(listPostsByKeyWords, {keyword: keyword, limit: limit}));
+            } else {
+                postData = await API.graphql(graphqlOperation(listPosts, {limit: limit}));
+            }
             // @ts-ignore
             const posts = postData.data.listPosts.items;
             posts.sort((a: { createdAt: string | number | Date | dayjs.Dayjs | null | undefined; }, b: { createdAt: string | number | Date | dayjs.Dayjs | null | undefined; }) => dayjs(b.createdAt).valueOf() - dayjs(a.createdAt).valueOf());
             /* postsから、contentにsearchWordsArrayの単語を含むもののみをfilterする */
-            const filteredPosts = posts.filter((post: any) => {
-                const content = post.content;
-                for (const searchWord of searchWordsArray) {
-                    if (content.includes(searchWord)) {
-                        continue;
-                    } else {
-                        return false;
+            if (searchMode === "and") {
+                console.log("and");
+                console.log(searchWordsArray);
+                const filteredPosts = posts.filter((post: any) => {
+                    const content = post.content;
+                    for (const searchWord of searchWordsArray) {
+                        if (content.includes(searchWord)) {
+                            continue;
+                        } else {
+                            return false;
+                        }
                     }
-                }
-                return true;
-            });
-            console.log(searchWordsArray);
-            console.log(filteredPosts);
+                    return true;
+                });
+                setPosts(filteredPosts);
+            } else {
+                console.log("or");
+                console.log(searchWordsArray)
+                const filteredPosts = posts.filter((post: any) => {
+                    const content = post.content;
+                    for (const searchWord of searchWordsArray) {
+                        if (content.includes(searchWord)) {
+                            return true;
+                        }
+                    }
+                    return false;
+                });
+                setPosts(filteredPosts);
+            }
           
-          setPosts(filteredPosts);
         } catch (err) {
           console.error('Error fetching posts', err);
         }
@@ -64,6 +100,18 @@ const Search = () => {
                     onChange={(e) => setSearchWords(e.target.value)}
                     sx={{m: 1}} fullWidth/>
                 <Button sx={{m: 1}} variant="contained" onClick={handleSearch}>検索</Button>
+            </Box>
+            <Box sx={{m: 2}}>
+                <FormControl>
+                    <FormLabel>検索設定</FormLabel>
+                    <RadioGroup
+                        row
+                        value={searchMode}
+                        onChange={handleSearchModeChange}>
+                        <FormControlLabel value="and" control={<Radio />} label="AND" />
+                        <FormControlLabel value="or" control={<Radio />} label="OR" />
+                    </RadioGroup>
+                </FormControl>
             </Box>
 
             <Box sx={{m: 0}}>
