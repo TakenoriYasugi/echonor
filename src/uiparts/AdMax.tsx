29acfb1dd4a217fb5fcc3309c0bc24e069b9fcc0
TakenoriYasugi <takenori.yasugi.cs@gmail.com>
2023-12-03 21:16:36 +0900

    広告の動的配置

--- src/uiparts/AdMax.tsx
+++ src/uiparts/AdMax.tsx
@@ -1,18 +1,21 @@
-// 忍者AdMaxの広告を表示するコンポーネント。
-// オリジナルのタグは以下の通り。
-// <!-- admax -->
-// <script src="https://adm.shinobi.jp/s/ea492f162cbda4ac8d8bf88a20cb6eb4"></script>
-// <!-- admax -->
-// これをReactで書き直す。
-
-import React from "react";
+import React, { useEffect } from 'react';
 
 const AdMax = () => {
-  return /*#__PURE__*/React.createElement("div", {
-    className: "admax"
-  }, /*#__PURE__*/React.createElement("script", {
-    src: "https://adm.shinobi.jp/s/ea492f162cbda4ac8d8bf88a20cb6eb4"
-  }));
-};
+  useEffect(() => {
+    const script = document.createElement('script');
+    script.src = "https://adm.shinobi.jp/s/ea492f162cbda4ac8d8bf88a20cb6eb4";
+    script.async = true;
+    document.body.appendChild(script);
+
+    return () => {
+      document.body.removeChild(script);
+    }
+  }, []);
+
+  return (
+    <div>
+    </div>
+  );
+}
 
 export default AdMax;
