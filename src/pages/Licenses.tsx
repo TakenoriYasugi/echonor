bff08e77e0b6c478eb99b97ee873461755fa4572
TakenoriYasugi <takenori.yasugi.cs@gmail.com>
2023-12-04 16:53:50 +0900

    ライセンス原文のリンクを配置

--- src/pages/Licenses.tsx
+++ src/pages/Licenses.tsx
@@ -38,6 +38,21 @@ const LicenseInfo: React.FC = () => {
   return (
     <Box>
       <Typography variant='h5' sx={{m: 2}}>ライセンス情報</Typography>
+      <Typography variant='body1' sx={{m: 2}}>
+        EchoNorの開発にはオープンソースソフトウェアを使用しています。
+      </Typography>
+
+      <div>
+        <ul>
+          <li><Link href="https://opensource.org/licenses/MIT" target="_blank">MIT License</Link></li>
+          <li><Link href="https://www.apache.org/licenses/LICENSE-2.0" target="_blank">Apache License 2.0</Link></li>
+          <li><Link href="https://opensource.org/licenses/ISC" target="_blank">ISC License</Link></li>
+          <li><Link href="https://opensource.org/licenses/BSD-2-Clause" target="_blank">BSD 2-Clause License</Link></li>
+          <li><Link href="https://opensource.org/licenses/BSD-3-Clause" target="_blank">BSD 3-Clause License</Link></li>
+          <li><Link href="https://opensource.org/licenses/Python-2.0" target="_blank">Python License 2.0</Link></li>
+          <li><Link href="https://www.mozilla.org/en-US/MPL/2.0/" target="_blank">Mozilla Public License 2.0</Link></li>
+        </ul>
+      </div>
       <ul>
         {licenses.map((license, index) => (
           <li key={index}>
