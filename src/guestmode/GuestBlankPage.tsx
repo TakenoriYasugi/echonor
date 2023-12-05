49816c84b4a099f70dd632608875db3655ee43be
TakenoriYasugi <takenori.yasugi.cs@gmail.com>
2023-12-05 16:48:29 +0900

    注釈を部品化して置き換え

--- src/guestmode/GuestBlankPage.tsx
+++ src/guestmode/GuestBlankPage.tsx
@@ -1,12 +1,11 @@
 import { Box, Typography } from "@mui/material";
+import RemarksCard from "../uiparts/RemarksCard";
 
 const GuestBlankPage = () => {
     return (
-        <Box sx={{m:1, p: 2, top: 50, left: 0, right: 0, borderRadius: "10px", backgroundColor: "#cceb69"}}>
-            <Typography fontSize={12} textAlign={"center"}>
-                このページはログインすると使用できます。
-            </Typography>
-        </Box>
+        <RemarksCard>
+            このページはログインすると使用できます。
+        </RemarksCard>
     );
 };
 
