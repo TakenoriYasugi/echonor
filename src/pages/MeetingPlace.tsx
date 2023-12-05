a3155f20829ca35267ff38c5816a12c72e4bafde
TakenoriYasugi <takenori.yasugi.cs@gmail.com>
2023-12-05 16:46:14 +0900

    集会場ページ仮置き

--- src/pages/MeetingPlace.tsx
+++ src/pages/MeetingPlace.tsx
@@ -1,9 +1,19 @@
-import { Typography } from "@mui/material";
+import { Box, Typography } from "@mui/material";
+import RemarksCard from "../uiparts/RemarksCard";
 
 const MeetingPlace = () => {
     return (
         <>
-            <Typography>集会場</Typography>
+            <Box sx={{m: 2}}>
+                <Typography variant="h6">集会場</Typography>
+            </Box>
+
+            <Box sx={{m: 2}}>
+                <RemarksCard>
+                    集会場では１つのトピックについて、ユーザー同士で自由に会話をすることができます。
+                    以下のルールを守って、楽しく交流しましょう。
+                </RemarksCard>
+            </Box>
         </>
     );
 }
